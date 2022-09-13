539c2ddbdef37c84f523eccb7027258609d6e953
sakimotorin <sakimotorin@gmail.com>
2022-09-13 18:05:21 +0800

    add three section

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,25 @@
 $(function() {
   console.log('Hello Bootstrap5');
 });
+
+
+var swiper = new Swiper(".course-swiper", {
+  autoplay: {
+      // disableOnInteraction: false,
+      // delay: 0
+  },
+  // speed: 1500,
+  loop: true,
+  spaceBetween: 16,
+  slidesPerView: 3,
+  //多欄
+  breakpoints: {
+      768: {
+          slidesPerView: 6
+      },
+      992: {
+          slidesPerView: 9
+      }
+  }
+
+});

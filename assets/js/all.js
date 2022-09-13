3e48ef36aaab521f3f7a346a08a3f84b59735e72
sakimotorin <sakimotorin@gmail.com>
2022-09-13 18:06:18 +0800

    Update 2022-09-13T10:05:56.903Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -3,4 +3,22 @@
 $(function () {
   console.log('Hello Bootstrap5');
 });
+var swiper = new Swiper(".course-swiper", {
+  autoplay: {// disableOnInteraction: false,
+    // delay: 0
+  },
+  // speed: 1500,
+  loop: true,
+  spaceBetween: 16,
+  slidesPerView: 3,
+  //多欄
+  breakpoints: {
+    768: {
+      slidesPerView: 6
+    },
+    992: {
+      slidesPerView: 9
+    }
+  }
+});
 //# sourceMappingURL=all.js.map
